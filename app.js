607bbf861da447e4629be3b039d141cef109e44c
Jessica Fuselier <jessfuse@gmail.com>
2013-07-26 09:20:26 -0500

    put on github

---
+++ app.js
@@ -0,0 +1,187 @@
+// #3 
+//alert("Hi");
+
+//#5 
+//var name = prompt ("What's your name");
+  //console.log("Hi " + name);
+
+//#6
+// var movie = prompt ("What movie would you like to see?");
+// var time = prompt ("What time would you like to check for?");
+//   console.log(movie + time);
+//   var answer = prompt("would you like to go see " + movie + " at " + time + " ?")
+//   console.log(answer);
+
+//#7
+// var password = prompt("What is your password?"); 
+
+// while (password.length < 10) {
+//     alert("Needs to be 10 or more characters");
+// } 
+// alert("Thank you");
+
+//#8
+// var age = prompt("How old are you?"); 
+//   if (age>=21) {
+//   alert("What can I get you?");
+// }
+// else {
+//   alert("I'm sorry, you can't drink");
+// }
+
+// var like = prompt("Do you like whiskey?"); 
+//   if (like == "yes") {
+//     alert("Hell yeah, you can drink!");
+//   }
+//   else {
+//     alert("nope, try again");
+//   }
+// There is more syntax included in JavaScript / var are set equal to a prompt instead of a value / there are no ends in JS
+
+
+//#9
+// function promptList() {
+//   array = [];
+//   for(j = 0; j < 10; j++) {
+//     list = prompt ("What do you need from the store?");
+//     array.push(list)}; 
+//     for (j in array) {
+//       console.log(array[j]);
+//   }
+//  alert("That's all folks!")
+//  promptList()
+
+//#10
+// myInfo = Object;
+// myInfo.first = "Jess";
+// myInfo.age = 24; 
+// myInfo.gender = "female";
+// myInfo.city = "Austin";
+// myInfo.career = "web dev";
+// myInfo.job = "recording studio manager";
+
+// alert("my name is " + myInfo.first + " I'm " + myInfo.age + " years old.  I'm a " + myInfo.gender + " I live in " + myInfo.city + " I'm learning to become a " + myInfo.career + " and I'm currently a " + myInfo.job + "." );
+
+//#11
+// function profile()
+ 
+
+
+
+
+//#16
+// $('h1:first');
+// $('h1');
+// $('div');
+// $('div.first');
+// $('.profilepic');
+// $('.nav-item:nth-child(1)');
+// $('.nav-item:nth-child(2)');
+// $('div:nth-child(4)');
+// $('div > p'); 
+
+//#17
+// $('.twitter-timeline').hide()
+
+//#18
+// $('.card').hide()
+
+//#19
+// $('.nav-item:nth-child(2)').show(); 
+
+//#20
+// $('.nav-item:nth-child(2)').text("hey there"); 
+
+//#21
+// $('.main p').append("<p> Hire me! </p>")
+
+//#22
+// $('.main p').after("<p> Hire me! I'm a passionate learner and a hard worker. </p>")
+
+//#23
+// $('.twitter-timeline').remove()
+
+//#24
+// theBlogThing = $('.nav-item').first().remove()
+// $('.nav-item').last().after(theBlogThing);
+
+//#25
+// $('.nav').clone().appendTo('.nav-item')[1];
+
+//#26
+// $('.profilepic').width(70); 
+
+//#27
+// // $('.twitter-timeline').hide(500);
+
+//#28 
+// $('.twitter-timeline').fadeIn(20);
+// $('.twitter-timeline').fadeOut(20);
+// $('.profilepic').height(20); 
+
+
+//#31
+//$('.main').hide();
+
+//#35
+//$('.main').click(function(){
+  //alert('Hey you')
+//})
+
+//#36
+//$('.main').click(function(){
+  //$('.main').append('!');
+//})
+
+//#37
+//$('body').click(function(){
+  //var e = $(this);
+  //e.css('background-color', 'red');
+//})
+
+//#38
+//$('.main').click(function(){
+  //$('.main').clone().appendTo('.main');
+//})
+
+//#40
+//$('.main').click(function(){
+  //var e = $(this);
+  //e.css("font-weight","bold");
+//})
+
+//var message = 'Boo!';
+//$('.main').bind('click', function() {
+  //alert(message);
+//});
+
+//$('.main').click(function(){
+  //$('.main').toggle();
+//})
+
+//#41
+// $(':submit').click(function() {
+//   $('div.error').empty();
+//   $('div.success').empty();
+//   var find = document.getElementById("email").value
+//   event.validate();
+//   if(document.getElementById("email").value.length < 8) {
+//     $('div.error').append("must be at least 8 characters long");
+//   }
+// });
+//Need help with this one 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
